3fbb9f47a1476dd6484e4a09eee0ce56f12ead05
AntMan <xanthonyalvarezx@gmail.com>
2021-01-05 12:03:34 -0500

    added footer icons for brands and linked them to corresponding pages

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,46 @@
 import homeComponent from "../components/Home";
 import "../stylesheets/Home.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookSquare,
+  faInstagramSquare,
+  faTwitter,
+  faYoutube,
+  faTwitch,
+  faDiscord,
+  faPatreon,
+} from "@fortawesome/free-brands-svg-icons";
+
 const Home = () => {
   return (
     <>
       <main>
         <homeComponent />
       </main>
-      <footer></footer>
+      <footer>
+        <a href="https://www.facebook.com/katsugg" target="_blank">
+          <FontAwesomeIcon className="footerIcon" icon={faFacebookSquare} />
+        </a>
+        <a href="https://www.twitter.com/cutletgg" target="_blank">
+          <FontAwesomeIcon className="footerIcon" icon={faTwitter} />
+        </a>
+        <a href="https://www.instagram.com/katsu.gg" target="_blank ">
+          <FontAwesomeIcon className="footerIcon" icon={faInstagramSquare} />
+        </a>
+        <a href="https://www.youtube.com/channel/UCNnfG-upnF3USXzfuAOMnRw" target="_blank ">
+          <FontAwesomeIcon className="footerIcon" icon={faYoutube} />
+        </a>
+        <a href="https://www.twitch.tv/katsugg" target="_blank ">
+          <FontAwesomeIcon className="footerIcon" icon={faTwitch} />
+        </a>
+        <a href="https://discordapp.com/invite/gF7CsVy" target="_blank ">
+          <FontAwesomeIcon className="footerIcon" icon={faDiscord} />
+        </a>
+        <a href="https://www.patreon.com/katsugg" target="_blank ">
+          <FontAwesomeIcon className="footerIcon" icon={faPatreon} />
+        </a>
+      </footer>
     </>
   );
 };
