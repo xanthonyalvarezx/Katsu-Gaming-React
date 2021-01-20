2e379ccb969b821cfe5f9e7ab05be35eff585867
Anthony Alvarez <anthonyalvarez@Anthonys-iMac.home>
2021-01-20 12:29:18 -0500

    finished about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,8 +1,9 @@
+import "../stylesheets/About.css";
 const About = () => {
   return (
-    <div id="homeBanner">
-      <h3 id="homeBannerText">Our Mission</h3>
-      <p>
+    <div id="about">
+      <h3 id="aboutBannerText">Our Mission</h3>
+      <p id="missionStatement">
         We made Katsu Gaming on a whim but our desire to improve the state of
         esports by making an organization and creating a brand that people can
         not only show off but be able to represent and become a part of is
